29b0981cc3ab768c1b5140243c635543c1f56502
Anas azkoul <anas@Anass-MacBook-Air.local>
2022-08-03 16:53:41 +0200

    added dark-mode feature

--- src/App.js
+++ src/App.js
@@ -2,8 +2,45 @@ import React, { useState, useEffect } from 'react';
 import data from './data';
 import Article from './Article';
 
-function App() {
-  return <h2>dark mode starter</h2>;
+const getStorageTheme = () => {
+  let theme = 'light-theme'; 
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme')
+  }
+  return theme; 
+}
+
+function App () {
+  const [theme, setTheme] = useState(getStorageTheme());
+  
+  useEffect(() => {
+    document.documentElement.className = theme; 
+    localStorage.setItem('theme', theme)
+  }, [theme])
+  
+  const handleToggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme')
+    } else {
+      setTheme('light-theme')
+    }
+  }
+  
+  return <main>
+    <nav>
+      <div className="nav-center">
+        <h1>overreacted</h1>
+        <button className="btn" onClick={handleToggleTheme}>
+          Toggle
+        </button>
+      </div>
+    </nav>
+    <section className="articles">
+      {data.map(item => {
+        return <Article key={item.id} {...item}/>
+      })}
+    </section>
+  </main>;
 }
 
 export default App;
